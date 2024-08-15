ec6fde98acc4ec6b702c0ae89edde984749bcafe
Facundo <js1697436@gmail.com>
2024-08-15 15:03:32 -0300

    Creación de carpetas para programación 2

---
+++ Programacion2/Matrices/src/Ejercicios/Ejercicio1.java
@@ -0,0 +1,14 @@
+package Ejercicios;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+
+		@Before
+	}
+
+}
---
+++ Programacion2/Matrices/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Matrices {
+	requires junit;
+}
