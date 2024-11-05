9e11f779787e0089cc78dc7071cfb5d629cf97af
Facundo Sosa <js1697436@gmail.com>
2024-11-04 22:01:09 -0300

    actualización del dia

---
+++ Programacion2/TP Final/parte2/TP_Final/src/bondiJET/Asiento.java
@@ -0,0 +1,24 @@
+package bondiJET;
+
+public class Asiento {
+
+    int numeroDeAsiento;
+    boolean estaLibre;
+
+    public int obtenerNumeroDeAsiento(){
+        
+        return 0;
+    }
+
+    public void asignarAsiento(){
+        
+    }
+
+    @Override
+    public String toString() {
+        
+        return "" + numeroDeAsiento + "";
+
+    }
+
+}
---
+++ Programacion2/TP Final/parte2/TP_Final/src/bondiJET/Cliente.java
@@ -0,0 +1,37 @@
+package bondiJET;
+
+import java.util.*;
+
+public class Cliente {
+
+    public Cliente(int dni, String nombre, String telefono){
+
+        if(dni > 0) this.dni = dni;
+        if(nombre.length() > 0) this.nombre = nombre;
+        if(telefono.length() > 0) this.telefono = telefono;
+
+    }
+
+    int dni;
+    String nombre;
+    String telefono;
+
+    public int getDni(){
+        
+        return dni;
+
+    }
+
+    public String ObtenerNombre(){
+
+        return nombre;
+
+    }
+
+    public String ObtenerTelefono(){
+
+        return telefono;
+
+    }
+
+}
